08ef43e12acd688670254e227dcc4084a01d53c9
xiewei05 <xiewei05@58.com>
2021-03-26 19:31:03 +0800

    注释scheme获取

--- .github/scripts/set-env-from-xcodeproj.sh
+++ .github/scripts/set-env-from-xcodeproj.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-SCHEME="$(xcodebuild -list -json | jq -r '.project.schemes[0]')"
-PRODUCT_NAME="$(xcodebuild -scheme "$SCHEME" -showBuildSettings | grep " PRODUCT_NAME " | sed "s/[ ]*PRODUCT_NAME = //")"
+# SCHEME="$(xcodebuild -list -json | jq -r '.project.schemes[0]')"
+# PRODUCT_NAME="$(xcodebuild -scheme "$SCHEME" -showBuildSettings | grep " PRODUCT_NAME " | sed "s/[ ]*PRODUCT_NAME = //")"
 # echo "::set-env name=PRODUCT_NAME::$PRODUCT_NAME"
 # echo "PRODUCT_NAME=$PRODUCT_NAME" >> $GITHUB_ENV
